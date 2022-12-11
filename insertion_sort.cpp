e6671abc68c8e3083a939c969b99800147a270d4
Suman Khadka <dnatsu022@gmail.com>
2022-12-11 22:02:19 +0545

    Insertion sort algorithm

---
+++ insertion_sort.cpp
@@ -0,0 +1,42 @@
+// program implementing insertion sort
+
+#include<iostream>
+using namespace std;
+
+void insertionSort(int arr[], int n)
+{
+    for(int i=0; i<n; i++)
+    {
+        int temp = arr[i];
+        int j = i-1;
+        while (j >=0 && arr[j]>temp)
+        {
+            arr[j+1] = arr[j];
+            j = j-1;
+        }
+        arr[j+1] = temp;
+    }
+    cout<<"\nSorted Array is : \n";
+    for (int i=0; i<n; i++)
+    {
+        cout<<"-> "<<arr[i];
+    }
+}
+int main()
+{
+    int n;
+    cout<<"Enter the number of elements in the array: ";
+    cin>>n;
+    int a[n];
+    cout<<"\nEnter the elements : \n";
+    for (int i=0; i<n; i++)
+    {
+        cin>>a[i];
+    }
+    cout<<"\nUnsorted array : \n";
+    for (int i=0; i<n; i++)
+    {
+        cout<<"-> "<<a[i];
+    }
+    insertionSort(a, n);
+}

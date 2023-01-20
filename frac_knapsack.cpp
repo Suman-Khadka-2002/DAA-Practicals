ac9784ba96e9dd3bd358240de76e0c1876af76ad
Suman Khadka <dnatsu022@gmail.com>
2023-01-20 20:29:59 +0545

    fractional knapsack problem

--- frac_knapsack.cpp
+++ frac_knapsack.cpp
@@ -1,64 +1,76 @@
-/* program to implement fractional knapsack problem using greedy programming */
 #include<iostream>
 using namespace std;
-int main()
-{
-    int array[2][100], n, w, i, curw, used[100], maxi = -1, totalprofit = 0;
-    //input number of objects
-    cout << "Enter number of objects: ";
-    cin >> n;
-    //input max weight of knapsack
-    cout << "Enter the weight of the knapsack: ";
-    cin >> w;
-    /* Array's first row is to store weights
-     second row is to store profits */
-    for (i = 0; i < n; i++)
+
+float weight[20],profits[20],maxProfit=0.0,fraction[20];
+int numberofitems,i,capacity,temp,j,remainingCapacity;
+
+class FractionalKnapsack {
+ public:
+  void getData() {
+    cout<<"Enter the maximum weight capacity of the bag: ";
+    cin>>capacity;
+    cout<<"Enter the number of objects you want to store inside the bag: ";
+    cin>>numberofitems;
+    cout<<"Enter the weight of the objects: ";
+    for(i=1;i<=numberofitems;i++)
     {
-        cin >> array[0][i] >> array[1][i];
+      cin>>weight[i];
     }
-    for (i = 0; i < n; i++)
+    cout<<"Enter the profit per unit weight of the objects: ";
+    for(i=1;i<=numberofitems;i++)
     {
-        used[i] = 0;
+      cin>>profits[i];
     }
-    curw = w;
-    //loop until knapsack is full
-    while (curw >= 0)
-    {
-        maxi = -1;
-        //loop to find max profit object
-        for (i = 0; i < n; i++)
-        {
-            if ((used[i] == 0) && ((maxi == -1) || (((float) array[1][i]
-                    / (float) array[0][i]) > ((float) array[1][maxi]
-                    / (float) array[0][maxi]))))
-            {
-                maxi = i;
-            }
+  }
+  void sortData() {
+    for(i=1;i<=numberofitems;i++) {
+      for(j=1;j<=numberofitems;j++) {
+        if((profits[j]/weight[j]) < (profits[j+1]/weight[j+1])) {
+          temp = profits[j];
+          profits[j] = profits[j+1];
+          profits[j+1] = temp;
+          temp = weight[j];
+          weight[j] = weight[j+1];
+          weight[j+1] = temp;
         }
-        used[maxi] = 1;
-        //decrease current wight
-        curw -= array[0][maxi];
-        //increase total profit
-        totalprofit += array[1][maxi];
-        if (curw >= 0)
-        {
-            cout << "\nAdded object " << maxi + 1 << " Weight: "
-                    << array[0][maxi] << " Profit: " << array[1][maxi]
-                    << " completely in the bag, Space left: " << curw;
-        }
-        else
-        {
-            cout << "\nAdded object " << maxi + 1 << " Weight: "
-                    << (array[0][maxi] + curw) << " Profit: "
-                    << (array[1][maxi] / array[0][maxi]) * (array[0][maxi]
-                            + curw) << " partially in the bag, Space left: 0"
-                    << " Weight added is: " << curw + array[0][maxi];
-            totalprofit -= array[1][maxi];
-            totalprofit += ((array[1][maxi] / array[0][maxi]) * (array[0][maxi]
-                    + curw));
-        }
+      }
     }
-    //print total worth of objects filled in knapsack
-    cout << "\nBags filled with objects worth: " << totalprofit;
-    return 0;
+  }
+  void calculation() {
+    for(i=0;i<numberofitems;i++) {
+      fraction[i] = 0.0;
+    }
+    remainingCapacity = capacity;
+    for(i=0;i<numberofitems;i++) {
+      if(weight[i] > remainingCapacity) {
+        break;
+      }
+      fraction[i] = 1.0;
+      remainingCapacity = remainingCapacity - weight[i];
+    }
+    if(i<numberofitems) {
+      fraction[i] = remainingCapacity / weight[i];
+    }
+    for(i=0;i<numberofitems;i++) {
+      maxProfit = maxProfit + (profits[i] * fraction[i]);
+    }
+  }
+  void displayData() {
+    cout<<"-------------------------------------------\n";
+    cout<<"Item No.\t Weight\t Profits\t Fraction\n";
+    cout<<"-----------------------------------------------\n";
+    for(i=1;i<=numberofitems;i++) {
+      cout<<i<<"\t\t"<<weight[i]<<"\t"<<profits[i]<<"\t\t"<<fraction[i]<<endl;
+    }
+    cout<<"MAXIMUM PROFIT: "<<maxProfit;
+  }
+};
+
+int main() {
+  FractionalKnapsack obj;
+  obj.getData();
+  obj.sortData();
+  obj.calculation();
+  obj.displayData();
+  return 0;
 }
